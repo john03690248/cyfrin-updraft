4a8edbdc2b4845802914775f35143a99f85226ca
Johnson <john03690248@gmail.com>
2024-10-29 13:02:27 +0800

    Update FundMe.sol

--- Solidity Smart Contract Development/FundMe.sol
+++ Solidity Smart Contract Development/FundMe.sol
@@ -6,15 +6,17 @@
 
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
 
-    uint public minimumUsd = 5;
+    uint public minimumUsd = 5e18;
     
     function fund() public payable {
         // Allow users to sen $
         // Have a minimnm $ sent $5
         // 1. How do we send ETH to this contract?
-        require(msg.value >= minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
+        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
 
         // What is a revert?
         // Undo any actions that have been done, and send the remaining gas back
@@ -22,9 +24,23 @@ contract FundMe {
 
     // function withdraw() public{}
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256){
         // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         // ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (, int256 price, , ,) = priceFeed.latestRoundData();
+        // Price of ETH in terms of USD
+        // 2000.00000000
+        return uint256(price * 1e10);
     }
-    function getConversionRate() public {}
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() public view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
 }
