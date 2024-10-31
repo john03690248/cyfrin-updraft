3b5fcbd46473ef3492c72531a88dbf2c270834da
Johnson <john03690248@gmail.com>
2024-10-31 14:35:15 +0800

    Update

--- Solidity Smart Contract Development/FundMe.sol
+++ Solidity Smart Contract Development/FundMe.sol
@@ -6,45 +6,30 @@
 
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
-    uint public minimumUsd = 5e18;
+    uint256 public minimumUsd = 5e18;
     
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable {
-        // Allow users to sen $
-        // Have a minimnm $ sent $5
-        // 1. How do we send ETH to this contract?
-        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
+        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH"); // 1e18 = 1 ETH = 1000000000000000000 = 1 * 10 ** 18
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
         // What is a revert?
         // Undo any actions that have been done, and send the remaining gas back
     }
-
-    // function withdraw() public{}
-
-    function getPrice() public view returns (uint256){
-        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        // ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price, , ,) = priceFeed.latestRoundData();
-        // Price of ETH in terms of USD
-        // 2000.00000000
-        return uint256(price * 1e10);
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
-
-    function getVersion() public view returns (uint256) {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    function withdraw() public {
+        // for loop
+        // [1, 2, 3, 4] elements
+        //  0, 1, 2, 3  indexes
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
     }
 }
